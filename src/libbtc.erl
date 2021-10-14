a93fe60731d74bd2b19c72b24e7eca1287a77412
Ferenc Pesti <fpesti@mail.datatrans.hu>
2021-10-14 15:48:46 +0200

    adding NIF loading

--- src/libbtc.erl
+++ src/libbtc.erl
@@ -2,8 +2,22 @@
 -export([derive/2]).
 -on_load(init/0).
 
+-define(APPNAME, libbtc).
+-define(LIBNAME, libbtc_nif).
+
 init() ->
-    ok = erlang:load_nif("./libbtc", 0).
+    SoName = case code:priv_dir(?APPNAME) of
+        {error, bad_name} ->
+            case filelib:is_dir(filename:join(["..", priv])) of
+                true ->
+                    filename:join(["..", priv, ?LIBNAME]);
+                _ ->
+                    filename:join([priv, ?LIBNAME])
+            end;
+        Dir ->
+            filename:join(Dir, ?LIBNAME)
+    end,
+    erlang:load_nif(SoName, 0).
 
 derive(_, _) ->
     exit(nif_library_not_loaded).
